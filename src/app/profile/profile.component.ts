be5bac8ba5d5e9c9dd8164ca6d73103f1664033a
neville <kilonzoneville@gmail.com>
2018-06-19 10:43:26 +0300

    add the profile function finding

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,3 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+   profile: any[];
+   repos: any[];
+   username:string;
+
+  constructor(private profileService: ProfileService) {
+
+   }
+
+   findProfile(){
+     this.profileService.updateProfile(this.username);
      this.profileService.getProfileInfo().subscribe(profile => {
        console.log(profile);
        this.profile = profile;
@@ -6,3 +25,9 @@
        console.log(repos);
        this.repos = repos;
      })
+   }
+
+  ngOnInit() {
+  }
+
+}
