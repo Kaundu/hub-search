fdc8964071b6c1c5dc1bc6aa8c705299a34b24c0
neville <kilonzoneville@gmail.com>
2018-06-19 10:25:16 +0300

    get clientid and clientsecret

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -6,4 +6,7 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ProfileService {
+  private username:string;
+  private clientid = '4d45ebac631954ab7281' ;
+  private clientsecret = '7fd8a57ff44ac2ac8085218b2b5cabe89e75f64';
 }
