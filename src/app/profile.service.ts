54eb54a641467d39f6bfbd94e4cee1e4a8ea05d0
neville <kilonzoneville@gmail.com>
2018-06-19 10:17:10 +0300

    Add profile service

---
+++ src/app/profile.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+}
